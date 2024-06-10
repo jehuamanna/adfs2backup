2d9043f524308ca87fc766acccf6527dca445dd8
jehu shalom amanna <jehuamanna@gmail.com>
2024-06-10 14:59:47 +0530

    base64

--- index.js
+++ index.js
@@ -18,13 +18,16 @@ const translations = {
 function parseJwt (token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(this.atob(base64).split('').map(function(c) {
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 
     return JSON.parse(jsonPayload);
 }
 
+function atob(s) {
+    return Buffer.from(s, "base64").toString()
+}
 
 let authToken = {
 
