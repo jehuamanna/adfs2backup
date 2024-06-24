26a6775299d762f766579dd76c0924480a9953fc
jehu shalom amanna <jehuamanna@gmail.com>
2024-06-24 18:53:36 +0530

    aded meta

--- src/api/oAuthRouter/controller.js
+++ src/api/oAuthRouter/controller.js
@@ -58,38 +58,47 @@ const oAuthAuth = (req, res) => {
     // SEND THE TOKEN TO THE CLIENT
 
     const html_ = `
+    <html>
+      <head>
+      <meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://adfs2.vercel.com ">
+
+      </head>
+      <body>
+    
     <div id="output"> </div>
-    <script>
+    <script >
     function sendTokenToParent(token) {
-        /* window.opener.postMessage(token, window.location.origin); */
-        /*window.opener.postMessage(token, "https://testingadfs-bhgjyz8fh-jehus-projects.vercel.app/"); */
-        /* window.opener.postMessage(token, "https://dev-frplus.dtdc.com/"); */
-        window.opener.postMessage(token, "http://localhost:1234");
-        window.opener.postMessage(token, "https://frplus-uat.dtdc.com" );
-        window.opener.postMessage(token, "https://frplus-dev.dtdc.com");
-        window.opener.postMessage(token, "https://dev-frplus.dtdc.com");
-        window.close();
-        console.log(token);
+      /* window.opener.postMessage(token, window.location.origin); */
+      /*window.opener.postMessage(token, "https://testingadfs-bhgjyz8fh-jehus-projects.vercel.app/"); */
+      /* window.opener.postMessage(token, "https://dev-frplus.dtdc.com/"); */
+      window.opener.postMessage(token, "http://localhost:1234");
+      window.opener.postMessage(token, "https://frplus-uat.dtdc.com" );
+      window.opener.postMessage(token, "https://frplus-dev.dtdc.com");
+      window.opener.postMessage(token, "https://dev-frplus.dtdc.com");
+      window.close();
+      console.log(token);
     }
-
+    
     const url = new URL(window.location.href);
-
+    
     // Get the query parameters
     const params = new URLSearchParams(url.search);
-
+    
     // Get specific parameters
     const param = params.get('token');
-
+    
     // Send the token to the parent window
     sendTokenToParent(param);
     console.log(token);
-
+    
     // Output the parameters
     document.getElementById('output').innerHTML = \`
-      <p>Token: \${param}</p>
+    <p>Token: \${param}</p>
     \`;
     </script>
-`;
+    </body>
+    </html>
+    `;
 
     return res.send(html_);
   } catch (error) {
