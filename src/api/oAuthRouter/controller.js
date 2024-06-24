831545d340710af1ed8adff13d96a0bbb09ae666
jehu shalom amanna <jehuamanna@gmail.com>
2024-06-24 20:22:44 +0530

    changes

--- src/api/oAuthRouter/controller.js
+++ src/api/oAuthRouter/controller.js
@@ -107,27 +107,19 @@ const oAuthDashboard = (req, res) => {
   try {
     logger.info('Called health-check endpoint');
     // SEND THE TOKEN TO THE CLIENT
+
     const html_ = `
-    <div id="output"> </div>
+    
       <script>
       function sendTokenToParent(token) {
         /* window.opener.postMessage(token, window.location.origin); */
         /*window.opener.postMessage(token, "https://testingadfs-bhgjyz8fh-jehus-projects.vercel.app/"); */
         /* window.opener.postMessage(token, "https://dev-frplus.dtdc.com/"); */
-        const parentWindow = window.opener || window.parent;
-console.log(parentWindow);
-        if (parentWindow) {
-          // Send the token to the parent window using postMessage
-          parentWindow.postMessage(token, "http://localhost:1234");
-          parentWindow.postMessage(token, "https://frplus-uat.dtdc.com" );
-          parentWindow.postMessage(token, "https://frplus-dev.dtdc.com");
-          parentWindow.postMessage(token, "https://dev-frplus.dtdc.com");
-          /**window.close();**/
-        } else {
-          console.error('Parent window is not available.');
-        }
-
-        
+        window.opener.postMessage(token, "http://localhost:1234");
+        window.opener.postMessage(token, "https://frplus-uat.dtdc.com" );
+        window.opener.postMessage(token, "https://frplus-dev.dtdc.com");
+        window.opener.postMessage(token, "https://dev-frplus.dtdc.com");
+        window.close();
   
         console.log(token);
       };
