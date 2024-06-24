1c66f3a1c93aba58b7e3ec7fc6ac1bc75c748c81
jehu shalom amanna <jehuamanna@gmail.com>
2024-06-24 19:55:34 +0530

    changes

--- src/api/oAuthRouter/controller.js
+++ src/api/oAuthRouter/controller.js
@@ -115,11 +115,20 @@ const oAuthDashboard = (req, res) => {
         /* window.opener.postMessage(token, window.location.origin); */
         /*window.opener.postMessage(token, "https://testingadfs-bhgjyz8fh-jehus-projects.vercel.app/"); */
         /* window.opener.postMessage(token, "https://dev-frplus.dtdc.com/"); */
-        window.opener.postMessage(token, "http://localhost:1234");
-        window.opener.postMessage(token, "https://frplus-uat.dtdc.com" );
-        window.opener.postMessage(token, "https://frplus-dev.dtdc.com");
-        window.opener.postMessage(token, "https://dev-frplus.dtdc.com");
-        window.close();
+        const parentWindow = window.opener || window.parent;
+
+        if (parentWindow) {
+          // Send the token to the parent window using postMessage
+          window.opener.postMessage(token, "http://localhost:1234");
+          window.opener.postMessage(token, "https://frplus-uat.dtdc.com" );
+          window.opener.postMessage(token, "https://frplus-dev.dtdc.com");
+          window.opener.postMessage(token, "https://dev-frplus.dtdc.com");
+          /**window.close();**/
+        } else {
+          console.error('Parent window is not available.');
+        }
+
+        
   
         console.log(token);
       };
